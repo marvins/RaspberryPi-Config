f66519eb7b39162062f4ef9c3acdd82481dc86a8
Marvin Smith <marvin_smith1@me.com>
2016-03-23 19:12:19 -0700

    Adding turret code baseline.

---
+++ turret/core/Options.py
@@ -0,0 +1,11 @@
+
+#  Python Libraries
+import argparse
+
+
+class Options:
+
+    def __init__(self):
+
+        pass
+
---
+++ turret/turret-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#  Turret Libraries
+from core.Options import Options
+
+# ----------------------------- #
+# -     Main Application      - #
+# ----------------------------- #
+def Main():
+
+    #  Parse Command-Line Options
+    options = Options()
+
+
+
+if __name__ == '__main__':
+    Main()
