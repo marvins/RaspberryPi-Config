a780546ca568e87ce46477ea1c525e74c18f841e
Marvin Smith <marvin_smith1@me.com>
2016-04-11 00:55:47 +0000

    Updating cal

--- turret-cpp/scripts/calibrate.m
+++ turret-cpp/scripts/calibrate.m
@@ -25,10 +25,15 @@ num_points = size(plist)(1)/4;
 
 cnt=1
 A = ones(num_points,3)
+W = ones(num_points,2)
 for p = 1:8
     A(p,1) = plist(cnt)
     cnt += 1
     A(p,2) = plist(cnt)
+    cnt += 1
+    W(p,1) = plist(cnt)
+    cnt += 1
+    W(p,2) = plist(cnt)
     cnt += 1
 end
 
@@ -83,14 +88,12 @@ adfGeoTransform = [Xi(3,1), Xi(1,1), Xi(2,1), Yi(3,1), Yi(1,1), Yi(2,1)];
 [Wx2, Wy2] = Apply_GeoTransform( P(2,1), P(2,2), adfGeoTransform );
 [Wx3, Wy3] = Apply_GeoTransform( P(3,1), P(3,2), adfGeoTransform );
 [Wx4, Wy4] = Apply_GeoTransform( P(4,1), P(4,2), adfGeoTransform );
-[Wx5, Wy5] = Apply_GeoTransform( P(5,1), P(5,2), adfGeoTransform );
 
 
 printf('W1: %d, %d from %d %d, Expect: %d %d\n', Wx1, Wy1, P(1,1), P(1,2), W(1,1), W(1,2));
 printf('W2: %d, %d from %d %d, Expect: %d %d\n', Wx2, Wy2, P(2,1), P(2,2), W(2,1), W(2,2));
 printf('W3: %d, %d from %d %d, Expect: %d %d\n', Wx3, Wy3, P(3,1), P(3,2), W(3,1), W(3,2));
 printf('W4: %d, %d from %d %d, Expect: %d %d\n', Wx4, Wy4, P(4,1), P(4,2), W(4,1), W(4,2));
-printf('W5: %d, %d from %d %d, Expect: %d %d\n', Wx5, Wy5, P(5,1), P(5,2), W(5,1), W(5,2));
 
 [W1x,W1y] = Apply_GeoTransform( 0,  0, adfGeoTransform )
 [W2x,W2y] = Apply_GeoTransform( 0, 32, adfGeoTransform )
